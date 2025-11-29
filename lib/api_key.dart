87d062adb8f80e2daf308e5958300f21568812a2
Simrashafqat14 <simrashafqat14@gmail.com>
2025-11-29 21:32:51 +0500

    enter your api key in apikey.dart

--- lib/api_key.dart
+++ lib/api_key.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: constant_identifier_names
 
 class ApiKeys {
-  static const String geminiApiKey = "AIzaSyAVHyvsO9ud52Vv4uNCoWemZklSp2hEhV8";
+  static const String geminiApiKey = "Your_Gemini_Key";
 }
